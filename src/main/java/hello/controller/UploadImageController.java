722c2ce3229241e5175d25bd21ea426aa2de0752
Sergey Vorobyev <sevoro.sv@gmail.com>
2016-09-22 08:17:10 +0500

    Add start write UploadImageController.java

---
+++ src/main/java/hello/controller/UploadImageController.java
@@ -0,0 +1,13 @@
+package hello.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author Sergey Vorobyev
+ */
+
+@Controller
+public class UploadImageController {
+
+    //https://github.com/spring-guides/gs-uploading-files
+}
